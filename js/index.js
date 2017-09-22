bfd0190ecfc32643a8c1783379a90f83eeab7a31
chilingirov <vasko988@gmail.com>
2017-09-22 16:24:32 -0700

    First commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $("#search").focus(function() {
+        var full = $("#poster").has("img").length ? true : false;
+        if (full == false) {
+            $("#poster").empty();
+        }
+    });
+
+    var getPoster = function() {
+        var film = $("#search").val();
+        if (film == "") {
+            $("#poster").html('<h2 class="loading">Please enter something in the form</h2>');
+        } else {
+            $("#poster").html("<h2 class='loading'>Your poster is loading</h2>");
+            $.getJSON("http://api.themoviedb.org/3/Movie.search/en/json/23afca60ebf72f8d88cdcae2c4f31866/" + film + "?callback=?", function(json) {
+                if (json != "Nothing found.") {
+                    //Display the poster and a message announcing the result
+                    $("#poster").html('<h2 class="loading" >We found you a poster</h2><img id="thePoster" src=' + json[0].posters[0].image.url + '/>');
+                } else {
+                    $.getJSON("http://api.themoviedb.org/3/Movie.search/en/json/23afca60ebf72f8d88cdcae2c4f31866/goonies?callback=?", function(json) {
+                        $("#poster").html('<h2 class="loading">Nothing found</h2>')
+                    });
+                }
+            });
+        }
+        return false;
+    }
+
+    $("#button").click(getPoster);
+    $("#search").keyup(function(e) {
+        if (e.keyCode == 13) {
+            getPoster();
+        }
+    });
+
+
+});
