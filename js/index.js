07da4ccfd73ea0e248fe00547af2b571b22e4994
chilingirov <vasko988@gmail.com>
2017-09-22 23:11:23 -0700

    Finished the project

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    var mykey = config.KEY;
     $("#search").focus(function() {
         var full = $("#poster").has("img").length ? true : false;
         if (full == false) {
@@ -6,30 +8,28 @@ $(document).ready(function() {
         }
     });
 
-    var getPoster = function() {
+    var getMovie = function() {
         var film = $("#search").val();
         if (film == "") {
             $("#poster").html('<h2 class="loading">Please enter something in the form</h2>');
         } else {
             $("#poster").html("<h2 class='loading'>Your poster is loading</h2>");
-            $.getJSON("http://api.themoviedb.org/3/Movie.search/en/json/somekey/" + film + "?callback=?", function(json) {
-                if (json != "Nothing found.") {
+            $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + mykey + "&query=" + film + "&callback=?", function(json) {
+                if (json.total_results != 0) {
                     //Display the poster and a message announcing the result
-                    $("#poster").html('<h2 class="loading" >We found you a poster</h2><img id="thePoster" src=' + json[0].posters[0].image.url + '/>');
+                    $("#poster").html('<div class="column-left"><img src=http://image.tmdb.org/t/p/w342/' + json.results[0].poster_path + ' /></div><div class="column-right"><h2>' + json.results[0].title + '</h2><p>' + json.results[0].overview + '</p><div class="rating"><h2> Rating:   ' + json.results[0].vote_average + '</h2><h2> Votes:   ' + json.results[0].vote_count + '</h2><h2><a target="_blank" href=https://www.themoviedb.org/movie/' + json.results[0].id + ' >Link</a></h2></div>');
                 } else {
-                    $.getJSON("http://api.themoviedb.org/3/Movie.search/en/json/somekey/goonies?callback=?", function(json) {
-                        $("#poster").html('<h2 class="loading">Nothing found</h2>')
-                    });
+                    $("#poster").html('<h2 class="loading">Nothing found</h2>');
                 }
             });
         }
         return false;
     }
 
-    $("#button").click(getPoster);
+    $("#button").click(getMovie);
     $("#search").keyup(function(e) {
         if (e.keyCode == 13) {
-            getPoster();
+            getMovie();
         }
     });
 
