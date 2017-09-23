7b61f780faefabf745e011158140e3b838496f8b
chilingirov <vasko988@gmail.com>
2017-09-22 17:43:14 -0700

    Edited index.js

--- js/index.js
+++ js/index.js
@@ -12,12 +12,12 @@ $(document).ready(function() {
             $("#poster").html('<h2 class="loading">Please enter something in the form</h2>');
         } else {
             $("#poster").html("<h2 class='loading'>Your poster is loading</h2>");
-            $.getJSON("http://api.themoviedb.org/3/Movie.search/en/json/23afca60ebf72f8d88cdcae2c4f31866/" + film + "?callback=?", function(json) {
+            $.getJSON("http://api.themoviedb.org/3/Movie.search/en/json/somekey/" + film + "?callback=?", function(json) {
                 if (json != "Nothing found.") {
                     //Display the poster and a message announcing the result
                     $("#poster").html('<h2 class="loading" >We found you a poster</h2><img id="thePoster" src=' + json[0].posters[0].image.url + '/>');
                 } else {
-                    $.getJSON("http://api.themoviedb.org/3/Movie.search/en/json/23afca60ebf72f8d88cdcae2c4f31866/goonies?callback=?", function(json) {
+                    $.getJSON("http://api.themoviedb.org/3/Movie.search/en/json/somekey/goonies?callback=?", function(json) {
                         $("#poster").html('<h2 class="loading">Nothing found</h2>')
                     });
                 }
